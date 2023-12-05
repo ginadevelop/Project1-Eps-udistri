45346ee910d89f684faa4233c6e1398d8e834e77
giainadev <gaardilaz@unal.edu.co>
2023-12-04 19:58:12 -0500

    Actualización msjs de errores

--- src/error_messages/messages.ts
+++ src/error_messages/messages.ts
@@ -1,5 +1,5 @@
 export const responseSuccess = (res: Response|any, data: any,code:Number):void => {
-    const SUCCESS_MESSAGE = 'Successful operation!'; 
+    const SUCCESS_MESSAGE = 'Operación exitosa!'; 
     res.status(code).json({
       message: SUCCESS_MESSAGE,
       data: data,
@@ -7,14 +7,14 @@ export const responseSuccess = (res: Response|any, data: any,code:Number):void =
   };
   
 export const errorResponseFailed = (res: Response|any):void =>{
-    const FAILURE_MESSAGE = 'The resource was not found'
+    const FAILURE_MESSAGE = 'El recurso no fue encontrado'
     res.status(404).json({ 
         message: FAILURE_MESSAGE 
       });
 };
 
 export const sendErrorServer = (res:Response|any, err:any) =>{
-    const ERROR_MESSAGE = 'An error has occurred on the server';
+    const ERROR_MESSAGE = 'Ocurrió un error en el servidor';
     res.status(500).json({
         message:ERROR_MESSAGE,
         error: err.message
